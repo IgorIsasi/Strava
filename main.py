e4cea1441a3711c2affd40cf7019378519900f80
Marru11 <markelmontejo@gmail.com>
2021-11-03 11:41:33 +0100

    athlethe activities

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ import time
 
 if __name__ == '__main__':
     stravaApiKud.getAccessToTheAPI()
-    print(stravaApiKud.getAthlete())
 
-    today = datetime.datetime.today()
-    yesterday=today-datetime.timedelta(days=1) 
+    egunak=int(input("Duela zenbat eguneko entrenamenduak ikusi nahi dituzu? "))
+    today = datetime.datetime.today() #Momentuko data eta ordua
+    yesterday=today-datetime.timedelta(days=egunak) #Atzoko data eta ordua
     yesterday=yesterday.timestamp() #Atzoko timestamp
-    parametroak={'after':yesterday, 'per_page':3}# 'before':50, 'page':1, 'per_page':5}
-    print(stravaApiKud.getActivities(parametroak))
+    parametroak={'after':yesterday}# 'before':50, 'page':1, 'per_page':5}
+    r=stravaApiKud.getActivities(parametroak)
+    print("-------------------------------------------------")
+    for entrenamendua in r:
+        print("Izena: ",entrenamendua['name'])
+        print("Data: ",entrenamendua['start_date_local'])
+        print("Mota: ",entrenamendua['type'])
+        print("Ikusgarritasuna: ",entrenamendua['visibility'])
+        print("Distantzia: ", entrenamendua['distance'])
+        print("Batazbesteko abiadura: ", entrenamendua['average_speed'])
+        print("Abiadura maximoa: ",entrenamendua['max_speed'])
+        print("Pultsometroa: ",entrenamendua['has_heartrate'])
+        print("-------------------------------------------------")
 
