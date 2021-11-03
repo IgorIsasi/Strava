3686044d070aced568e9ce5bd77217ef4b291811
IgorIsasi <92370391+IgorIsasi@users.noreply.github.com>
2021-11-03 11:12:33 +0100

    Add files via upload
    yep

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from controllers.StravaAPI import stravaApiKud
-import datetime
-import time
+
 
 if __name__ == '__main__':
     stravaApiKud.getAccessToTheAPI()
     print(stravaApiKud.getAthlete())
-
-    today = datetime.datetime.today()
-    yesterday=today-datetime.timedelta(days=1) 
-    yesterday=yesterday.timestamp() #Atzoko timestamp
-    parametroak={'after':yesterday, 'per_page':3}# 'before':50, 'page':1, 'per_page':5}
-    print(stravaApiKud.getActivities(parametroak))
-
+    em = stravaApiKud.getActivities()
+    ac = stravaApiKud.getActivityId(em[0]["id"])
+    print(ac["name"], ac["type"], ac["distance"])
