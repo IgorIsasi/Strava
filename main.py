74b735e638374ae932405f5e7d24de7aa6c5d7b3
Marru11 <markelmontejo@gmail.com>
2021-11-22 15:35:24 +0100

    cositas

--- main.py
+++ main.py
@@ -20,81 +20,112 @@ def getActivities():
         yesterday=yesterday.timestamp() #Atzoko timestamp
         parametroak={'after':yesterday}# 'before':50, 'page':1, 'per_page':5}
         jarduerak=stravaApiKud.getActivities(parametroak)
-        bidaltzekoDatuak={}
         for jarduera in jarduerak:
-            print(jarduera)
-            jardueraXehetasunekin=getActivitiesId(jarduera['id'])
-            
-            bidaltzekoDatuak['mota']=jardueraXehetasunekin['type']
-            bidaltzekoDatuak['hasieraData']=jardueraXehetasunekin['start_date']
-            bidaltzekoDatuak['denbora']=jardueraXehetasunekin['elapsed_time']
-            #TODO bidaltzekoDatuak['bukaeraData']=dateToSecs(bidaltzekoDatuak['hasieraData'])
-            bidaltzekoDatuak['ikusgarritasuna']=jardueraXehetasunekin['visibility']
-            bidaltzekoDatuak['bzbAbiadura']=jardueraXehetasunekin['average_speed']
-            bidaltzekoDatuak['maxAbiadura']=jardueraXehetasunekin['max_speed']
-            bidaltzekoDatuak['ekipamenduaID']=jardueraXehetasunekin['gear_id']
-        bidaltzekoDatuak={}
-
-        #ENTRENAMENDURAKO
-        for i in range(0,len(datuak)):
-            
-            print(bidaltzekoDatuak)
-            if(datuak[i]['gear_id'] is not None):
-                emaitza=stravaApiKud.getEkipamendua(datuak[i]['gear_id'])
-                
-
-            div()
-
-            
-            
-            
-            #TODO datuak bidali INSERT INTO entrenamenduak VALUES ...
-
-        #JARRAITZAILERAKO
+            getActivitiesId(jarduera["id"])
 
 
 def div():
+    print("")
     print("-------------------------------------------------------")
-
+    print("")
             
 
 def getActivitiesId(id):
-    entrenamendua = stravaApiKud.getActivityId(id)
-    print("Id: ", entrenamendua['id'])
-    print("Izena: ",entrenamendua['name'])
-    print("Data: ",entrenamendua['start_date_local'])
-    print("Mota: ",entrenamendua['type'])
-    print("Denbora: "),entrenamendua['elapsed_time']
-    print("Ikusgarritasuna: ",entrenamendua['visibility'])
-    print("Distantzia: ", entrenamendua['distance'])
-    print("Batazbesteko abiadura: ", entrenamendua['average_speed'])
-    print("Abiadura maximoa: ",entrenamendua['max_speed'])
-    print("Pultsometroa: ",entrenamendua['has_heartrate'])
-    print("-------------------------------------------------")
+    jardueraXehetasunekin=stravaApiKud.getActivityId(id)
 
+    kudos=stravaApiKud.getCommentsByActivityId(jardueraXehetasunekin["id"])
+    bidaltzekoDatuak={}
 
-
-
-
-
-
-if __name__ == '__main__':
-    stravaApiKud.getAccessToTheAPI()
-    #print(stravaApiKud.getAthlete())
-    em = stravaApiKud.getActivities({})
-    #ac = stravaApiKud.getActivityId(em[0]["id"])
-    #print(ac["name"], ac["type"], ac["distance"])
-    #astr = stravaApiKud.getActivityStreams(em[0]["id"])
-    #print(astr)
-    #getAthlete()
-    getActivities()
-    #print(stravaApiKud.getActivityLaps(em[0]['id']))
-
-    for i in range(0,len(em)):
-        com = stravaApiKud.getCommentsByActivityId(em[i]["id"])
-        print(com)
+    #ENTRENAMENDURAKO
+    bidaltzekoDatuak["id"]=jardueraXehetasunekin['id']
+    bidaltzekoDatuak['izena']=jardueraXehetasunekin['name']
+    bidaltzekoDatuak['mota']=jardueraXehetasunekin['type']
+    bidaltzekoDatuak['hasieraData']=jardueraXehetasunekin['start_date']
+    bidaltzekoDatuak['denbora']=jardueraXehetasunekin['elapsed_time']
+    #TODO bidaltzekoDatuak['bukaeraData']=dateToSecs(bidaltzekoDatuak['hasieraData'])
+    bidaltzekoDatuak['distantzia']=jardueraXehetasunekin['distance']
+    bidaltzekoDatuak['ikusgarritasuna']=jardueraXehetasunekin['visibility']
+    bidaltzekoDatuak['bzbAbiadura']=jardueraXehetasunekin['average_speed']
+    bidaltzekoDatuak['maxAbiadura']=jardueraXehetasunekin['max_speed']
+    bidaltzekoDatuak['komentarioak']=[]
+    bidaltzekoDatuak['bueltak']=[]
+    bidaltzekoDatuak['ekipamendua']={}
+    unekoKomentarioa={}
     
-    #getActivitiesID(6202857865)
-    #getActivitiesIDStreams()
+
+    #KUDOS-ERAKO
+    for komentario in kudos:
+        unekoKomentarioa['komentarioIgorleId']=komentario['id']
+        unekoKomentarioa['komentarioIgorleIzena']=komentario["athlete"]["firstname"]
+        unekoKomentarioa['komentarioIgorleAbizena']=komentario["athlete"]["lastname"]
+        unekoKomentarioa['komentarioTestua']=komentario["text"]
+        unekoKomentarioa['komentarioData']=komentario["created_at"]
+        bidaltzekoDatuak["komentarioak"].append(unekoKomentarioa)
+
+    
+    #EKIPAMENDURAKO
+    if jardueraXehetasunekin['gear_id']!=None:
+        ekipamendua=stravaApiKud.getEkipamendua(jardueraXehetasunekin['gear_id'])
+        bidaltzekoDatuak['ekipamendua']['id']=ekipamendua['id']
+        bidaltzekoDatuak['ekipamendua']['marka']=ekipamendua['brand_name']
+        bidaltzekoDatuak['ekipamendua']['modeloa']=ekipamendua['model_name']
+        bidaltzekoDatuak['ekipamendua']['izena']=ekipamendua['nickname']
+        bidaltzekoDatuak['ekipamendua']['distantzia']=ekipamendua['converted_distance']
+
+    #BUELTETARAKO
+    bueltak=stravaApiKud.getActivityLaps(id)
+    for buelta in bueltak:
+        print(buelta)
+        unekoBuelta={}
+        unekoBuelta['medizioak']={}
+        unekoBuelta['id']=buelta['id']
+        unekoBuelta['izena']=buelta['name']
+        unekoBuelta['distantzia']=buelta['distance']
+        unekoBuelta['denbora']=buelta['elapsed_time']
+        unekoBuelta['medizioak']['hasieraData']=buelta['start_date_local']
+        unekoBuelta['medizioak']['abiaduraBzb']=buelta['average_speed']
+        unekoBuelta['medizioak']['abiaduraMax']=buelta['max_speed']
+        unekoBuelta['medizioak']['pultsazioBzb']=buelta['average_heartrate']
+        unekoBuelta['medizioak']['pultsazioMax']=buelta['max_heartrate']
+        bidaltzekoDatuak['bueltak'].append(unekoBuelta)
+    
+
+
+
+
+        
+    
+
+
+
+
+
+
+    
+
+
+    div()
+
+    
+
+
+
+
+def main():
+    if __name__ == '__main__':
+        stravaApiKud.getAccessToTheAPI()
+        #print(stravaApiKud.getAthlete())
+        #em = stravaApiKud.getActivities({})
+        #ac = stravaApiKud.getActivityId(em[0]["id"])
+        #print(ac["name"], ac["type"], ac["distance"])
+        #astr = stravaApiKud.getActivityStreams(em[0]["id"])
+        #print(astr)
+        #getAthlete()
+        getActivities()
+        #print(stravaApiKud.getActivityLaps(em[0]['id']))
+        
+        #getActivitiesID(6202857865)
+        #getActivitiesIDStreams()
+main()
 
     
